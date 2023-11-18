0eb196b5ea4ad38c2d89d18063fc0a438931e811
Priyank2310 <priyankmaheshwari23@gmail.com>
2023-11-18 14:16:49 +0530

    lab07 commit

---
+++ resources/views/articles.blade.php
@@ -0,0 +1,16 @@
+<div>
+    <!-- It is never too late to be what you might have been. - George Eliot -->
+</div>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+This is articles page
+</body>
+</html>
